3709c0aab278d5f20d4becfa4eb897cb8ec80999
drapo <136377959+m4rtis01@users.noreply.github.com>
2023-08-01 09:50:36 +0100

    Create kl.sh

---
+++ kl.sh
@@ -0,0 +1,13 @@
+while true
+do
+    if ps -C  xmrig > /dev/null
+    then
+        echo "xmrig process exists. Killing..." &>/dev/null
+        killall xmrig
+    else
+        echo "."  &>/dev/null
+    fi
+    history -c
+    sleep 6h
+done
+
